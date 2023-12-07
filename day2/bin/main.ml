076f39dd292691b94f2908ad1acdb018a92eb388
pr0me <g33sus@gmail.com>
2023-12-07 13:24:16 +0100

    day2 part1 done

--- day2/bin/main.ml
+++ day2/bin/main.ml
@@ -6,9 +6,11 @@ let process_line line content =
   let r_max = ref 0 in
   let g_max = ref 0 in
   let b_max = ref 0 in
+  Printf.printf "[i] Initial RMAX: %d\n" !r_max;
   let groups = Str.split (Str.regexp "; ") content in
   ignore (List.iter (fun group ->
     let values = Str.split (Str.regexp ", ") group in
+    ignore (List.iter(fun v -> Printf.printf "[i] Group split: %s\n" v) values);
     let get_value color = 
       try
         let color_assign = List.find (fun s -> 
@@ -18,7 +20,7 @@ let process_line line content =
           with Not_found -> false
         ) values in
         let color_value = List.hd (Str.split (Str.regexp " ") color_assign) in
-        (* Printf.printf "Color Value: %s %s\n" color color_value; *)
+        Printf.printf "Color Value: %s %s\n" color color_value;
         int_of_string color_value
       with Not_found -> 0
     in
@@ -30,28 +32,27 @@ let process_line line content =
     if red_value > !r_max then r_max := red_value;
     if green_value > !g_max then g_max := green_value;
     if blue_value > !b_max then b_max := blue_value;
+    Printf.printf "    Max Red: %d; Max Green: %d; Max Blue: %d\n" !r_max !g_max !b_max;
 
   ) groups);
 
-  
-  
   if !r_max <= r_allowed && !g_max <= g_allowed && !b_max <= b_allowed then
     let _ = Printf.printf "[i] Adding game ID: %d\n" line in
-    Printf.printf "Max Red: %d\n" !r_max;
-    Printf.printf "Max Green: %d\n" !g_max;
-    Printf.printf "Max Blue: %d\n" !b_max;
+    Printf.printf "    Max Red: %d; Max Green: %d; Max Blue: %d\n" !r_max !g_max !b_max;
     line
   else 
     0
+
 
 let get_valid_games filename = 
   let ic = open_in filename in
   let rec loop i acc =
     match input_line ic with
-    | line -> loop (i + 1) (acc + ((process_line (i) line)))
+    | line -> loop (i + 1) (acc + ((process_line (i+1) line)))
     | exception End_of_file -> close_in ic; acc
   in
   loop 0 0
+
 
 let () = 
   let sum = get_valid_games "clean_games.txt" in
