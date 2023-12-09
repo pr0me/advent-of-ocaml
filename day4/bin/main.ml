5fd305a22104d16241e6fc9eefaf75ee06309abd
pr0me <g33sus@gmail.com>
2023-12-09 12:01:10 +0100

    day4 part1 done

--- day4/bin/main.ml
+++ day4/bin/main.ml
@@ -4,7 +4,7 @@ let process_line content =
 
   let parse_nums lst =
     let rec aux acc = function
-      | [] -> List.rev acc (* Reverse the accumulator to maintain the original order *)
+      | [] -> List.rev acc
       | hd :: tl ->
         try
           let num = int_of_string hd in
@@ -15,18 +15,40 @@ let process_line content =
     aux [] lst
   in
 
-  let sp_nums = parse_nums all_numbers in
-  ignore (List.iter (fun el -> Printf.printf "%d " (el)) sp_nums);
-  (* let winners = List.iter *)
+  let winners = parse_nums all_numbers in
+  let entries = List.rev (parse_nums (List.rev all_numbers)) in
+
+  ignore (List.iter (fun el -> Printf.printf "%d " (el)) winners);
   Printf.printf "\n";
-  
-  1
+  ignore (List.iter (fun el -> Printf.printf "%d " (el)) entries);
+  Printf.printf "-------------------------";
+  Printf.printf "\n";
+
+  let count_matches w e =
+    let length = List.length w in
+    let count_match count entry =
+      if List.mem entry w then
+        if count > length then
+          count
+        else
+          count + 1
+      else
+        count
+    in
+    List.fold_left count_match 0 e
+  in
+
+  let hits = count_matches winners entries in
+  if hits > 0 then
+    2.0 ** float_of_int (hits - 1)
+  else
+    0.0
 
 let get_valid_games filename = 
   let ic = open_in filename in
   let rec loop acc =
     match input_line ic with
-    | line -> loop (acc + ((process_line line)))
+    | line -> loop (acc + (int_of_float (process_line line)))
     | exception End_of_file -> close_in ic; acc
   in
   loop 0
